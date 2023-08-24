5d034a0c0bcf864cd449670f30b29faaa2cd1d0d
Kumar Harsh <114024228+harsh556@users.noreply.github.com>
2023-08-24 23:56:40 +0530

    Add files via upload

---
+++ hexa to dec.cpp
@@ -0,0 +1,18 @@
+//hexadecimal to decimal
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+/*int hexadecimaltodecimal(string a){
+	int ans=0;
+	int x=1;
+	s=a.size();
+	
+}*/
+
+int main(){
+	string x;
+	cout<<"Enter the hexadecimal"<<endl;
+	cin>>x;
+	cout<<x.size();
+	return 0;
+}
