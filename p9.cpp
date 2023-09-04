209db2f75be83eb088a0fe247551805bb2799472
Kumar Harsh <114024228+harsh556@users.noreply.github.com>
2023-09-04 22:34:43 +0530

    Add files via upload

---
+++ p9.cpp
@@ -0,0 +1,15 @@
+//Print Inverted Half Pyramid using numbers - 2
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"enter the size of pyramid pattern"<<endl;
+	cin>>n;
+	for(int i=n;i>=1;i--){
+		for(int j=1;j<=i;j++){
+			cout<<j<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
